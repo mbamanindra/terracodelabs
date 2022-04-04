f737c1254cc584f676b31db684e6e3c8708b1fbe
mbamanindra <mbamanindra@gmail.com>
2022-04-04 06:28:03 +0000

    tag changed

--- simple-instance.tf
+++ simple-instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ourfirst" {
   ami           = "ami-0b9b188c3820fc461"
   instance_type = "t3a.small"
   tags = { 
-       Name = "server-pipe"
+       Name = "server-pipeline"
        env = "development"
   }
 
