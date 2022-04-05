29208417183b88e9b79b6799ff8f5673939cdc01
root <root@ip-172-31-26-220.ap-southeast-1.compute.internal>
2022-04-05 04:50:59 +0000

    tag changed

--- simple-instance.tf
+++ simple-instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ourfirst" {
   ami           = "ami-0553264457fca256a"
   instance_type = "t2.medium"
   tags = { 
-       Name = "server-pipeline"
+       Name = "server-pipeline-1"
        env = "development"
   }
 
