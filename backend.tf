37896a8a95c40605eb25025e1939d6a022ad3e75
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-19 11:38:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "manindra-jenkins"
-    key    = "remote.tfstate"
-    region = "ap-southeast-1"
+    bucket = "manindra-jenkins1"
+    key    = "terraform/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
