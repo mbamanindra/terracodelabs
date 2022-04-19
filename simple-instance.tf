70d39ef6310b62e82a7810ecb4381337644b5f6a
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-19 12:05:57 +0530

    Update simple-instance.tf

--- simple-instance.tf
+++ simple-instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0d2986f2e8c0f7d01"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   tags = { 
        Name = "server-pipeline-1"
        env = "development"
