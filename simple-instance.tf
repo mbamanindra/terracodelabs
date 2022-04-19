0065e95e610d9250e4a8ffbabdb7d4fc6863103f
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-19 12:08:19 +0530

    Update simple-instance.tf

--- simple-instance.tf
+++ simple-instance.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0d2986f2e8c0f7d01"
   instance_type = "t2.medium"
+    subnet_id = "subnet-07bfb8a4dcffbeb02"
   tags = { 
        Name = "server-pipeline-1"
        env = "development"
